8180a573dd1504538d93655566df9d7b8c071d70
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-27 16:26:11 -0500

    Create input form and update the AddNewItem component

--- src/components/AddNewItem.tsx
+++ src/components/AddNewItem.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { AddItemButton } from '../styles';
+import { NewItemForm } from './NewItemForm';
 
 interface AddNewItemProps {
   onAdd(text: string): void;
@@ -12,7 +13,14 @@ export const AddNewItem = (props: AddNewItemProps) => {
   const { onAdd, toggleButtonText, dark } = props;
 
   if (showForm) {
-    // We show item creation form here }
+    return (
+      <NewItemForm
+        onAdd={(text: string) => {
+          onAdd(text);
+          setShowForm(false);
+        }}
+      />
+    );
   }
 
   return (
