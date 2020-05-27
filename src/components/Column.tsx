9edffd2cb8d22403545b8f1627b1d2b86539a790
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-27 11:36:44 -0500

    Render children inside the columns

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -5,10 +5,14 @@ interface ColumnProps {
   text: string;
 }
 
-export const Column = ({ text }: ColumnProps) => {
+export const Column = ({
+  text,
+  children,
+}: React.PropsWithChildren<ColumnProps>) => {
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
+      {children}
     </ColumnContainer>
   );
 };
