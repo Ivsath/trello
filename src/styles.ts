08e4d8fc37f33b26d01c70fc6311dfdf1178f7d7
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-27 12:15:47 -0500

    Add styles for the buttonw and the form

--- src/styles.ts
+++ src/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-// AppContainer - arrange columns horizontally
-// ColumnContainer - set the grey background and rounded corners
-// ColumnTitle - make column title bold and add paddings
-// CardContainer
+interface AddItemButtonProps {
+  dark?: boolean;
+}
 
+// Arrange columns horizontally
 export const AppContainer = styled.div`
   align-items: flex-start;
   background-color: #3179ba;
@@ -15,6 +15,7 @@ export const AppContainer = styled.div`
   width: 100%;
 `;
 
+// Set the grey background and rounded corners
 export const ColumnContainer = styled.div`
   background-color: #ebecf0;
   width: 300px;
@@ -25,6 +26,7 @@ export const ColumnContainer = styled.div`
   flex-grow: 0;
 `;
 
+// Make column title bold and add paddings
 export const ColumnTitle = styled.div`
   padding: 6px 16px 12px;
   font-weight: bold;
@@ -38,4 +40,47 @@ export const CardContainer = styled.div`
   max-width: 300px;
   border-radius: 3px;
   box-shadow: #091e4240 0px 1px 0px 0px;
+`;
+
+export const AddItemButton = styled.button<AddItemButtonProps>`
+  background-color: #ffffff3d;
+  border-radius: 3px;
+  border: none;
+  color: ${(props) => (props.dark ? '#000' : '#fff')};
+  cursor: pointer;
+  max-width: 300px;
+  padding: 10px 12px;
+  text-align: left;
+  transition: background 85ms ease-in;
+  width: 100%;
+  &:hover {
+    background-color: #ffffff52;
+  }
+`;
+
+export const NewItemFormContainer = styled.div`
+  max-width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: flex-start;
+`;
+
+export const NewItemButton = styled.button`
+  background-color: #5aac44;
+  border-radius: 3px;
+  border: none;
+  box-shadow: none;
+  color: #fff;
+  padding: 6px 12px;
+  text-align: center;
+`;
+
+export const NewItemInput = styled.input`
+  border-radius: 3px;
+  border: none;
+  box-shadow: #091e4240 0px 1px 0px 0px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
 `;
