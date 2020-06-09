33f765c1711f2bee6b7dc5e7b7cf487a312b2561
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-09 11:52:54 -0500

    Get data from AppStateContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,21 +2,18 @@ import React from 'react';
 
 import { AppContainer } from '../styles';
 import { Column } from './Column';
-import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
 
+import { useAppState } from '../context/AppStateContext';
+
 function App() {
+  const { state } = useAppState();
+
   return (
     <AppContainer>
-      <Column text="To Do">
-        <Card text="Generate app scaffold" />
-      </Column>
-      <Column text="In Progress">
-        <Card text="Learn Typescript" />
-      </Column>
-      <Column text="Done">
-        <Card text="Begin to use static typing" />
-      </Column>
+      {state.lists.map((list, i) => (
+        <Column text={list.text} key={list.id} index={i} />
+      ))}
       <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log} />
     </AppContainer>
   );
