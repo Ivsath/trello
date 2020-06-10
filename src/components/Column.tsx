03fec193fed34f7bf2426f9f0697d98dc4bde018
Irvin Reyes <reyes.yerp@gmail.com>
2020-06-10 00:08:12 -0500

    Add logic to move columns

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useDrop } from 'react-dnd';
+
 import { ColumnContainer, ColumnTitle } from '../styles';
 import { Card } from './Card';
 import { AddNewItem } from './AddNewItem';
 
 import { useAppState } from '../context/AppStateContext';
+import { useItemDrag } from '../hooks/useItemDrag';
+import { DragItem } from '../types/DragItem';
 
 interface ColumnProps {
   text: string;
@@ -13,9 +17,41 @@ interface ColumnProps {
 
 export const Column = ({ text, index, id }: ColumnProps) => {
   const { state, dispatch } = useAppState();
+  const ref = useRef<HTMLDivElement>(null);
+  // The hover callback is triggered whenever we move the dragged item above the drop target
+  const [, drop] = useDrop({
+    accept: 'COLUMN',
+    hover(item: DragItem) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+
+      // If we are hovering above the dragged item
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      // When we hover over another column we’ll dispatch a MOVE_LIST action to swap the dragged and target column positions
+      dispatch({
+        type: 'MOVE_LIST',
+        payload: {
+          dragIndex,
+          hoverIndex,
+        },
+      });
+      item.index = hoverIndex;
+    },
+  });
+  const { drag } = useItemDrag({
+    type: 'COLUMN',
+    id,
+    index,
+    text,
+  });
+
+  drag(drop(ref));
 
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref}>
       <ColumnTitle>{text}</ColumnTitle>
       {state.lists[index].tasks.map((task) => (
         <Card text={task.text} key={task.id} />
@@ -23,7 +59,13 @@ export const Column = ({ text, index, id }: ColumnProps) => {
       <AddNewItem
         toggleButtonText="+ Add another task"
         onAdd={(text) =>
-          dispatch({ type: 'ADD_TASK', payload: { text, taskId: id } })
+          dispatch({
+            type: 'ADD_TASK',
+            payload: {
+              text,
+              taskId: id,
+            },
+          })
         }
         dark
       />
